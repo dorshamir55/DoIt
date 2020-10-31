4ba4a929a0232e44a9eea9faf45ce799103afd54
Dor Shamir <dorshamir4@gmail.com>
2020-10-31 22:59:45 +0200

    Bottom toolbar

--- app/src/main/java/com/example/doit/ui/MainActivity.java
+++ app/src/main/java/com/example/doit/ui/MainActivity.java
@@ -15,6 +15,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
